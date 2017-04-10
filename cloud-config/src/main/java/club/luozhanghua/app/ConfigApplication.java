df0b916aaee159f23db8201ee9fdc643766859f0
romeo <romeo@listcloud.cn>
2017-04-10 18:01:22 +0800

    1、完善config 2、添加zuul

--- cloud-config/src/main/java/club/luozhanghua/app/ConfigApplication.java
+++ cloud-config/src/main/java/club/luozhanghua/app/ConfigApplication.java
@@ -2,6 +2,7 @@ package club.luozhanghua.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -9,8 +10,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
-public class Application {
+@EnableDiscoveryClient
+public class ConfigApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ConfigApplication.class, args);
     }
 }
