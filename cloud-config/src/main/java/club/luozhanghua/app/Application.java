a834b69da8c544cada2d4926d39b64e6ffd3e0d3
zhanghua.luo <zhanghualuo@gmail.com>
2017-04-17 23:17:25 +0800

    1.添加docker配置

--- cloud-config/src/main/java/club/luozhanghua/app/Application.java
+++ cloud-config/src/main/java/club/luozhanghua/app/Application.java
@@ -2,6 +2,7 @@ package club.luozhanghua.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
