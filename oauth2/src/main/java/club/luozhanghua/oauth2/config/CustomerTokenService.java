14d825ec77a005a5353191143d76472e2371c9d2
luozhanghua <lzh>
2018-10-29 17:05:00 +0800

    1、引入jedis 2、将(token, jwt) 缓存至token 3、将配置保存到application.yml

--- oauth2/src/main/java/club/luozhanghua/oauth2/config/CustomerTokenService.java
+++ oauth2/src/main/java/club/luozhanghua/oauth2/config/CustomerTokenService.java
@@ -2,6 +2,10 @@ package club.luozhanghua.oauth2.config;
 
 import club.luozhanghua.oauth2.utils.JwtUtil;
 
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
@@ -15,11 +19,15 @@ import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
  * @modified By
  */
 public class CustomerTokenService extends DefaultTokenServices {
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
     @Override
     public OAuth2AccessToken createAccessToken(OAuth2Authentication authentication) throws AuthenticationException {
         OAuth2AccessToken token = super.createAccessToken(authentication);
         String jwtToken = JwtUtil.createJwtToken((UserDetails) authentication.getPrincipal());
-
+        stringRedisTemplate.opsForValue().set(token.getValue(), jwtToken, 60 * 60 * 2, TimeUnit.SECONDS);
         return token;
     }
 
